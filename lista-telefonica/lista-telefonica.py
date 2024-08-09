a79374f4a91df5952ae138b2f70b8d583da43052
Gabriel <josegabrielas0586@gmail.com>
2024-08-09 12:06:55 -0300

    Mudanças no código da lista-telefonica

--- lista-telefonica/lista-telefonica.py
+++ lista-telefonica/lista-telefonica.py
@@ -1,38 +1,41 @@
+'''ADICIONAR'''
 def adicionar_contato(agenda, nome, telefone):
     if nome in agenda:
-        print(f'{nome} já está na sua agenda')
+        print('Este nome já existe na sua lista')
     else:
-        print(f'Adicionando {nome} na lista')
+        print(f'{nome} foi adicionado a lista')
         agenda[nome] = telefone
-        print(f'{agenda[nome]} foi salvo no contato {nome}')
+        print(f'{agenda[nome]} foi adicionado no contato {nome}')
 
+'''EXCLUIR'''
 def excluir_contato(agenda, nome):
     if nome in agenda:
         del agenda[nome]
-        print (f'{nome} foi removido da sua lista de contatos')
     else:
-        print (f'Não existe nenhum contato chamado {nome}')
+        print(f'O nome {nome} não se encontar na sua lista')
 
-def listar_contato(agenda, nome, telefone):
-    if not agenda:
-        print('Não exite nenhum contato na sua lista')
-    else:
-        print('Lista telefônica\n')
-        for nome, telefone in agenda.items():
-            print(f'Nome: {nome}, Telefone: {telefone}')
-        
+'''BUSCAR'''
 def buscar_contato(agenda, nome):
     if nome in agenda:
-        print(f'Nome: {nome}, Telefone: {agenda[nome]}')
+        print (f'\nNome: {nome}, Telefone: {agenda[nome]}')
     else:
-        (f'{nome} não encontrado')
+        print(f'\nNão existe {nome} na sua lista')
 
-def exibir_menu():
-    print('\nMenu')
+'''LISTAR'''
+def listar_contato(agenda, nome):
+    if not agenda:
+        print('\nNão existe ninguém na agenda\n')
+    else:
+        for nome, agenda[nome] in agenda.items():
+            print(f'Nome: {nome}, Telefone: {agenda[nome]}')
+
+'''MENU'''
+def menu_contato():
+    print('\n--MENU--')
     print('1. Adicionar Contato')
-    print('2. Excluir Contato')
-    print('3. Listar Contato')
-    print('4. Buscar Contato')
+    print('2. Excluir contato')
+    print('3. Buscar contato')
+    print('4. Listar contato')
     print('5. Sair')
 
 '''CÓDIGO PRINCIPAL'''
@@ -40,24 +43,24 @@ def agenda_telefonica():
     agenda = {}
 
     while True:
-        exibir_menu()
-        r = input('') 
-        if r == '1':
-            nome = input('Digite o nome do contato: ')
-            telefone = input('Digite o telefone: ')
+        menu_contato()
+        resposta = input ('Digite a opção:\n')
+        if resposta == '1':
+            nome = input('Digite o nome:\n')
+            telefone = input('Digite o telefone:\n')
             adicionar_contato(agenda, nome, telefone)
-        elif r == '2':
-            nome = input('Digite o nome que queira excluir: ')
+        if resposta == '2':
+            nome = input('Digite o nome:\n')
             excluir_contato(agenda, nome)
-        elif r == '3':
-            listar_contato(agenda, nome, telefone)
-        elif r == '4':
-            nome = input('Digite o nome do contato ')
+        if resposta == '3':
+            nome = input('Digite o nome:\n')
             buscar_contato(agenda, nome)
-        elif r == '5':
-            print('Saindo...\n')
+        if resposta == '4':
+            listar_contato(agenda, nome)
+        if resposta == '5':
+            print('\nSaindo...\n')
             break
         else:
-            print('Opção inválida! Tente novamente.\n')
+            print('Opção inválida, tente novamente...\n')
 
 agenda_telefonica()
